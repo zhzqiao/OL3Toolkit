dbc080a4144e65929bae8d71b0ee28f7747b52dc
zhzqiao <z.zqiao0810@gmail.com>
2016-05-06 21:17:39 +0800

    调整了进度条，开始添加其他地图源。偏差问题解决中

--- ol3toolkit.js
+++ ol3toolkit.js
@@ -1,4 +1,4 @@
-
+//地图定位控件，还存在问题
 ol.control.CustomZoomTo = function(opt_options){
     var options = opt_options || {};
 
@@ -12,7 +12,8 @@ ol.control.CustomZoomTo = function(opt_options){
         if($.OL3Toolkit.options.enalblePosition){
             $.OL3Toolkit.getLocation.activate();
         }else {
-            this_.getMap().getView().centerOn($.OL3Toolkit.options.viewCenter, this_.getMap().getSize(), [570,500]);
+            this_.getMap().getView().setCenter($.OL3Toolkit.options.viewCenter);
+            this_.getMap().getView().setZoom($.OL3Toolkit.options.zoomLevel);
         }
         e.preventDefault();
     };
@@ -61,7 +62,8 @@ ol.control.CustomZoomTo.prototype.getLocation = function (map, viewCenter) {
         var coordinates = geolocation.getPosition();
         positionFeature.setGeometry(coordinates ?
             new ol.geom.Point(coordinates) : null);
-        map.getView().centerOn(ol.proj.transform(coordinates,"EPSG:4326","EPSG:3857"));
+        map.getView().setCenter(ol.proj.transform(coordinates,"EPSG:4326","EPSG:3857"));
+        map.getView().setZoom($.OL3Toolkit.options.zoomLevel);
     });
 
     new ol.layer.Vector({
@@ -114,7 +116,7 @@ $.OL3Toolkit.options = {
     //初始地图缩放等级
     zoomLevel: 7,
     //底图数据源
-    baseMapSource: ["OSM",'SATELLITE'],
+    baseMapSource: ["OSM",'SATELLITE','QQ','BAIDU'],
     //自动把输入的'EPSG:4326'转换到'EPSG:3857/900913'
     autoLatLngTransform: true,
     //地图大小自适应
@@ -131,7 +133,7 @@ $.OL3Toolkit.options = {
     showScaleLine: true,
     //一个定位控件，优先当前位置，若不支持则回到初始视角
     //定位
-    enalblePosition: true,
+    enalblePosition: false,
     //回到初始视角
     backOriginView: true,
     //测量功能
@@ -183,6 +185,13 @@ $(function() {
     if(o.quickCreation){
         $.OL3Toolkit.createMap.activate();
     }
+    //如果包含百度地图则需动态调整
+    if(true){
+        ol3view.on('change:center',function(event){
+            map.getLayer().getSource().tileGrid = $.OL3Toolkit.mapSources.BAIDU.tilegrid();
+            // baiduSource.tileGrid = tilegrid();
+        })
+    }
 
     //由于采用的模板是almasaeed2010/AdminLTE
     //https://github.com/almasaeed2010/AdminLTE
@@ -219,8 +228,21 @@ $(function() {
 
 })
 
-
-
+//定义基本点
+function point(lng, lat) {
+    if (isStr(lng)) {
+        lng = parseFloat(lng)
+    }
+    if (isStr(lat)) {
+        lat = parseFloat(lat)
+    }
+    this.lng = lng;
+    this.lat = lat
+}
+//判断是否是字符串
+function isStr(T) {
+    return typeof T == "string"
+}
 /**
  * ----------------------
  * - 初始化OL3Toolkit对象 -
@@ -229,12 +251,154 @@ $(function() {
  * @private
  */
 function ol3ToolkitInit_() {
+    //坐标转换工具
+    $.OL3Toolkit.Translate = {};
+    $.OL3Toolkit.Translate.latlng = {
+        pi : 3.14159265358979324,
+        x_pi : 52.35987755982988, //pi * 3000.0 / 180.0,
+        a : 6378245.0,
+        ee : 0.00669342162296594323,
+        nor_mars: function (arg) {
+            var wgLon = arg[0];
+            var wgLat = arg[1];
+            var radLat = wgLat / 180.0 * this.pi;
+            var magic = Math.sin(radLat);
+            magic = 1 - this.ee * magic * magic;
+            var sqrtMagic = Math.sqrt(magic);
+            var dLon = this.transformLon(wgLon - 105.0, wgLat - 35.0);
+            var dLat = this.transformLat(wgLon - 105.0, wgLat - 35.0);
+            dLon = (dLon * 180.0) / (this.a / sqrtMagic * Math.cos(radLat) * this.pi);
+            dLat = (dLat * 180.0) / ((this.a * (1 - this.ee)) / (magic * sqrtMagic) * this.pi);
+            mLon = wgLon + dLon;
+            mLat = wgLat + dLat;
+            return [mLon,mLat];
+        },
+        mars_baidu: function (arg) {
+            var x = arg[0], y = arg[1];
+            var z = Math.sqrt(x * x + y * y) + 0.00002 * Math.sin(y * this.x_pi);
+            var theta = Math.atan2(y, x) + 0.000003 * Math.cos(x * this.x_pi);
+            bdLon = z * Math.cos(theta) + 0.0065;
+            bdLat = z * Math.sin(theta) + 0.006;
+            return [bdLon,bdLat];
+        },
+        baidu_mars: function (arg) {
+            var x = arg[0] - 0.0065, y = arg[1] - 0.006;
+            var z = Math.sqrt(x * x + y * y) - 0.00002 * Math.sin(y * this.x_pi);
+            var theta = Math.atan2(y, x) - 0.000003 * Math.cos(x * this.x_pi);
+            mgLon = z * Math.cos(theta);
+            mgLat = z * Math.sin(theta);
+            return [mgLon,mgLat];
+        },
+        transformLat: function (x, y) {
+            var ret = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * Math.sqrt(Math.abs(x));
+            ret += (20.0 * Math.sin(6.0 * x * this.pi) + 20.0 * Math.sin(2.0 * x * this.pi)) * 2.0 / 3.0;
+            ret += (20.0 * Math.sin(y * this.pi) + 40.0 * Math.sin(y / 3.0 * this.pi)) * 2.0 / 3.0;
+            ret += (160.0 * Math.sin(y / 12.0 * this.pi) + 320 * Math.sin(y * this.pi / 30.0)) * 2.0 / 3.0;
+            return ret;
+        },
+        transformLon: function (x, y){
+        var ret = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * Math.sqrt(Math.abs(x));
+        ret += (20.0 * Math.sin(6.0 * x * this.pi) + 20.0 * Math.sin(2.0 * x * this.pi)) * 2.0 / 3.0;
+        ret += (20.0 * Math.sin(x * this.pi) + 40.0 * Math.sin(x / 3.0 * this.pi)) * 2.0 / 3.0;
+        ret += (150.0 * Math.sin(x / 12.0 * this.pi) + 300.0 * Math.sin(x / 30.0 * this.pi)) * 2.0 / 3.0;
+        return ret;
+    }
+    };
+
+    $.OL3Toolkit.Translate.ll_Mercator = {
+        EARTHRADIUS: 6370996.81,
+        MCBAND: [12890594.86, 8362377.87, 5591021, 3481989.83, 1678043.12, 0],
+        LLBAND: [75, 60, 45, 30, 15, 0],
+        MC2LL: [
+            [1.410526172116255e-8, 0.00000898305509648872, -1.9939833816331, 200.9824383106796, -187.2403703815547, 91.6087516669843, -23.38765649603339, 2.57121317296198, -0.03801003308653, 17337981.2],
+            [-7.435856389565537e-9, 0.000008983055097726239, -0.78625201886289, 96.32687599759846, -1.85204757529826, -59.36935905485877, 47.40033549296737, -16.50741931063887, 2.28786674699375, 10260144.86],
+            [-3.030883460898826e-8, 0.00000898305509983578, 0.30071316287616, 59.74293618442277, 7.357984074871, -25.38371002664745, 13.45380521110908, -3.29883767235584, 0.32710905363475, 6856817.37],
+            [-1.981981304930552e-8, 0.000008983055099779535, 0.03278182852591, 40.31678527705744, 0.65659298677277, -4.44255534477492, 0.85341911805263, 0.12923347998204, -0.04625736007561, 4482777.06],
+            [3.09191371068437e-9, 0.000008983055096812155, 0.00006995724062, 23.10934304144901, -0.00023663490511, -0.6321817810242, -0.00663494467273, 0.03430082397953, -0.00466043876332, 2555164.4],
+            [2.890871144776878e-9, 0.000008983055095805407, -3.068298e-8, 7.47137025468032, -0.00000353937994, -0.02145144861037, -0.00001234426596, 0.00010322952773, -0.00000323890364, 826088.5]
+        ],
+        LL2MC: [
+            [-0.0015702102444, 111320.7020616939, 1704480524535203, -10338987376042340, 26112667856603880, -35149669176653700, 26595700718403920, -10725012454188240, 1800819912950474, 82.5],
+            [0.0008277824516172526, 111320.7020463578, 647795574.6671607, -4082003173.641316, 10774905663.51142, -15171875531.51559, 12053065338.62167, -5124939663.577472, 913311935.9512032, 67.5],
+            [0.00337398766765, 111320.7020202162, 4481351.045890365, -23393751.19931662, 79682215.47186455, -115964993.2797253, 97236711.15602145, -43661946.33752821, 8477230.501135234, 52.5],
+            [0.00220636496208, 111320.7020209128, 51751.86112841131, 3796837.749470245, 992013.7397791013, -1221952.21711287, 1340652.697009075, -620943.6990984312, 144416.9293806241, 37.5],
+            [-0.0003441963504368392, 111320.7020576856, 278.2353980772752, 2485758.690035394, 6070.750963243378, 54821.18345352118, 9540.606633304236, -2710.55326746645, 1405.483844121726, 22.5],
+            [-0.0003218135878613132, 111320.7020701615, 0.00369383431289, 823725.6402795718, 0.46104986909093, 2351.343141331292, 1.58060784298199, 8.77738589078284, 0.37238884252424, 7.45]
+        ],
+        convertMC2LL: function(cL) {
+            var cL = new point(cL[0],cL[1]);
+            var cM, cO;
+            cM = new point(Math.abs(cL.lng), Math.abs(cL.lat));
+            for (var cN = 0; cN < this.MCBAND.length; cN++) {
+                if (cM.lat >= this.MCBAND[cN]) {
+                    cO = this.MC2LL[cN];
+                    break
+                }
+            }
+            var T = this.convertor(cL, cO);
+            var cL = [T.lng, T.lat];
+            return cL
+        },
+        convertLL2MC: function(T) {
+            var T = new point(T[0],T[1]);
+            var cL, cN;
+            T.lng = this.getLoop(T.lng, -180, 180);
+            T.lat = this.getRange(T.lat, -74, 74);
+            cL = new point(T.lng, T.lat);
+            for (var cM = 0; cM < this.LLBAND.length; cM++) {
+                if (cL.lat >= this.LLBAND[cM]) {
+                    cN = this.LL2MC[cM];
+                    break
+                }
+            }
+            if (!cN) {
+                for (var cM = this.LLBAND.length - 1; cM >= 0; cM--) {
+                    if (cL.lat <= -this.LLBAND[cM]) {
+                        cN = this.LL2MC[cM];
+                        break
+                    }
+                }
+            }
+            var cO = this.convertor(T, cN);
+            var T = [cO.lng, cO.lat];
+            return T
+        },
+        convertor: function(cM, cN) {
+            if (!cM || !cN) {
+                return
+            }
+            var T = cN[0] + cN[1] * Math.abs(cM.lng);
+            var cL = Math.abs(cM.lat) / cN[9];
+            var cO = cN[2] + cN[3] * cL + cN[4] * cL * cL + cN[5] * cL * cL * cL + cN[6] * cL * cL * cL * cL + cN[7] * cL * cL * cL * cL * cL + cN[8] * cL * cL * cL * cL * cL * cL;
+            T *= (cM.lng < 0 ? -1 : 1);
+            cO *= (cM.lat < 0 ? -1 : 1);
+            return new point(T, cO)
+        },
+        getRange: function(cM, cL, T) {
+            if (cL != null) {
+                cM = Math.max(cM, cL)
+            }
+            if (T != null) {
+                cM = Math.min(cM, T)
+            }
+            return cM
+        },
+        getLoop: function(cM, cL, T) {
+            while (cM > T) {
+                cM -= T - cL
+            }
+            while (cM < cL) {
+                cM += T - cL
+            }
+            return cM
+        }
+    }
 
 
     $.OL3Toolkit.mapSources = {
         SATELLITE: function () {
             return new ol.layer.Tile({
-                title: '卫星数据',
+                title: '基础卫星',
                 type: 'base',
                 visible: false,
                 source: new ol.source.MapQuest({
@@ -246,8 +410,120 @@ function ol3ToolkitInit_() {
             return new ol.layer.Tile({
                 title: 'OSM',
                 type: 'base',
-                visible: true,
+                visible: false,
                 source: new ol.source.OSM()
+            });
+        },
+        QQ: function () {
+            var resolutions = [];
+            var attribution = new ol.Attribution({
+                html: 'Copyright:&copy; 2015 腾讯地图'
+            });
+            var projection = ol.proj.get('EPSG:3857');
+            var projectionExtent = projection.getExtent();
+
+            var tileSize = 256;
+
+            var maxResolution = ol.extent.getWidth(projectionExtent) / tileSize;
+            var z;
+            for (z = 0; z < 22; ++z) {
+                resolutions[z] = maxResolution / Math.pow(2, z);
+            }
+
+            var urlTemplate = 'http://p3.map.gtimg.com/maptilesv3/{z}/{qqx}/{qqy}/{x}_{y}.png?version=20150501';
+            var tilegrid = new ol.tilegrid.TileGrid({
+                origin: ol.extent.getBottomLeft(projectionExtent),
+                resolutions: resolutions,
+                tileSize: 256
+            });
+            var tilesource = new ol.source.TileImage({
+                attributions: [attribution],
+                projection: projection,
+                tileGrid: tilegrid,
+                tileUrlFunction: function (xyz, pixelRatio, projection) {
+                    if (!xyz) {
+                        return "";
+                    }
+                    var z = xyz[0];
+                    var x = xyz[1];
+                    var y = xyz[2];
+                    if (x < 0) {
+                        x = "M" + (-x);
+                    }
+                    if (y < 0) {
+                        y = "M" + (-y);
+                    }
+                    var qqx = Math.floor(x / 16.0);
+                    var qqy = Math.floor(y / 16.0);
+                    return urlTemplate.replace('{z}', z.toString())
+                        .replace('{y}', y.toString())
+                        .replace('{x}', x.toString())
+                        .replace('{qqx}', qqx.toString())
+                        .replace('{qqy}', qqy.toString());
+                }
+            });
+            return new ol.layer.Tile({
+                title: '腾讯地图',
+                type: 'base',
+                visible: true,
+                extent: projectionExtent,
+                source: tilesource
+            });
+        },
+        BAIDU: function(){
+            // 自定义分辨率和瓦片坐标系
+            var resolutions = [];
+            var maxZoom = 18;
+            var attribution = new ol.Attribution({
+                html: 'Copyright:&copy; 2015 百度地图'
+            });
+
+            // 计算百度使用的分辨率
+            for(var i=0; i<=maxZoom; i++){
+                resolutions[i] = Math.pow(2, maxZoom-i);
+            }
+            function tilegrid(){
+                var Convertor = $.OL3Toolkit.Translate;
+                return new ol.tilegrid.TileGrid({
+                    origin: (function() {
+                        //ol3view全局变量，这儿有点问题
+                        var epsg3857Center = ol3view.getCenter();
+                        var epsg4326Center = ol.proj.transform(epsg3857Center,'EPSG:3857','EPSG:4326');
+                        var baiduCoord = Convertor.ll_Mercator.convertLL2MC(epsg4326Center);
+                        var baiduCoord2 = ol.proj.transform(Convertor.latlng.nor_mars(Convertor.latlng.mars_baidu(epsg4326Center)),'EPSG:4326','EPSG:3857');
+                        return [2*epsg3857Center[0]-baiduCoord[0]-baiduCoord2[0],2*epsg3857Center[1]-baiduCoord[1]-baiduCoord2[1]]
+                    })(),    // 设置原点坐标
+                    resolutions: resolutions  // 设置分辨率
+                })
+            }
+            // 创建百度地图的数据源
+            var baiduSource = new ol.source.TileImage({
+                attributions: [attribution],
+                projection: 'EPSG:3857',
+                tileGrid: tilegrid(),
+                tileUrlFunction: function(tileCoord, pixelRatio, proj){
+                    var z = tileCoord[0];
+                    var x = tileCoord[1];
+                    var y = tileCoord[2];
+
+                    // 百度瓦片服务url将负数使用M前缀来标识
+                    if(x<0){
+                        x = 'M' + (-x);
+                    }
+                    if(y<0){
+                        y = 'M' + (-y);
+                    }
+
+                    return "http://online0.map.bdimg.com/onlinelabel/?qt=tile&x="+x+"&y="+y+"&z="+z+"&styles=pl&udt=20160426&scaler=1&p=0";
+                }
+            });
+
+            // 百度地图层
+            return new ol.layer.Tile({
+                title: '百度地图',
+                type: 'base',
+                visible: true,
+                source: baiduSource
             });
         }
     };
@@ -268,13 +544,14 @@ function ol3ToolkitInit_() {
             if($.OL3Toolkit.options.autoLatLngTransform && o.viewCenter[0] <= 180 && o.viewCenter[0] >= -180 && o.viewCenter[1] <= 90 && o.viewCenter[1] >= -90){
                 o.viewCenter = ol.proj.transform(o.viewCenter, 'EPSG:4326', 'EPSG:3857')
             }
+            ol3view = new ol.View({
+                center:o.viewCenter,
+                zoom: o.zoomLevel
+            });
             map = new ol.Map({
+                view: ol3view,
                 layers: this_.createLayers(o.baseMapSource),
-                target: o.targetID,
-                view: new ol.View({
-                    center:o.viewCenter,
-                    zoom: o.zoomLevel
-                })
+                target: o.targetID
             });
         },
         // 组装成最终的图层
@@ -289,7 +566,7 @@ function ol3ToolkitInit_() {
                 title: '叠加图层',
                 layers: [
                     new ol.layer.Tile({
-                        title: '国界',
+                        title: '省界',
                         source: new ol.source.TileWMS({
                             url: 'http://demo.opengeo.org/geoserver/wms',
                             params: {'LAYERS': 'ne:ne_10m_admin_1_states_provinces_lines_shp'},
@@ -337,7 +614,7 @@ function ol3ToolkitInit_() {
             });
         },
         fix: function (outerElem) {
-            map.setSize([outerElem.width(),$(window).height() - $('.main-footer').outerHeight() - $('.main-header').outerHeight() - 8])
+            map.setSize([outerElem.width(),$(window).height() - $('.main-footer').outerHeight() - $('.main-header').outerHeight() - 5])
         }
     };
 
@@ -383,10 +660,10 @@ function ol3ToolkitInit_() {
             if (this.loading === this.loaded) {
               this.loading = 0;
               this.loaded = 0;
-              var this_ = this;
+              /*var this_ = this;
               setTimeout(function() {
                 this_.hide();
-              }, 500);
+              }, 500);*/
             }
         },
         //显示进度条
@@ -397,11 +674,11 @@ function ol3ToolkitInit_() {
         hide: function() {
             if (this.loading === this.loaded) {
               this.el.style.visibility = 'hidden';
-              this.el.style.width = 0;
             }
         }
     };
 
+    //还存在问题
     $.OL3Toolkit.getLocation = {
         activate: function () {
             var geolocation = new ol.Geolocation();
